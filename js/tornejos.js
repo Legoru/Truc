36bc52345f4d8daf7a73d534f31ffc76899fc5f9
Legoruu <13arnauvidal@gmail.com>
2024-09-16 22:04:32 +0200

    frontend tournament almost finished

--- js/tornejos.js
+++ js/tornejos.js
@@ -5,15 +5,17 @@ function buildMonth() {
   const date = new Date();
   const actualYear = date.getFullYear();
   const actualMonth = date.getMonth();
+  const actualDay = date.getDate();
+  
   let firstLast = [
     new Date(actualYear, actualMonth, 1),
     new Date(actualYear, actualMonth + 1, 0),
   ];
+
   firstLast = firstLast.map((date) => ({
     weekDay: getDayPosition(date.toDateString().split(" ")[0]),
     absoluteDay: date.toDateString().split(" ")[2],
   }));
-  console.log("🚀 ~ firstLast=firstLast.map ~ firstLast:", firstLast);
 
   const calendar = document.querySelector(".calendari");
   const days = Array.from(calendar.children);
@@ -24,10 +26,14 @@ function buildMonth() {
     ) {
       day.classList.add("hiddenDaySeven");
     } else {
+      let tournamentsHtml = retrieveTournaments(day, actualMonth, actualYear);
+      if (count - firstLast[0].weekDay < actualDay - 1) {
+        day.classList.add("pastDay");
+      }
       day.innerHTML = `<div class="dayInfo">
             <span class="dayLetter">${getDayLetter(count % 7)}</span>
             <span class="dayNumber">${count - firstLast[0].weekDay + 1}</span>
-          </div>`
+          </div>`;
     }
   });
 }
@@ -42,4 +48,8 @@ function getDayLetter(day) {
 
 function getDayCalendarPosition(calendarDay) {
   return calendarDay.id.split("day")[1];
+}
+
+function retrieveTournaments(day, actualMonth, actualYear) {
+  if (day.id === "day17") console.log(day);
 }
